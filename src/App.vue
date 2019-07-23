be5e12a61e3c782e24bbef8a65e9f061033a13e2
Daniel H <daguiheso@gmail.com>
2019-07-22 23:28:25 -0500

    Create input search and add pending dependencies for pug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang="pug">
+  #app
+    section.section
+      nav
+        input(
+          type="text",
+          placeholder="search songs",
+          v-model="searchQuery"
+          )
+        a.button.info(@click="search") Buscar
+        a.button.danger &times;
+
 </template>
 
 <script>
@@ -10,12 +17,47 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello World'
+      searchQuery: ''
+    }
+  },
+  methods: {
+    search () {
+      console.log(this.searchQuery)
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    padding: 5%;
+  }
+  nav {
+    display: flex;
+    input {
+      height: 45px;
+      padding: 12px;
+      width: 100%;
+      border-radius: 6px;
+    }
+    a.button {
+      height: 45px;
+      padding: 0 22px;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 6px;
+      cursor: pointer;
+      &.info {
+        background: rgb(50, 60, 207);
+      }
+      &.danger {
+        background: rgb(233, 38, 71);
+      }
+    }
+  }
 </style>
