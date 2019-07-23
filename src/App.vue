1f095ea1282bfdfd960980a47b8b1b58a9842a82
Daniel H <daguiheso@gmail.com>
2019-07-23 17:00:39 -0500

    Mock tracks and computed result

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,41 @@
           type="text",
           placeholder="search songs",
           v-model="searchQuery"
-          )
+        )
         a.button.info(@click="search") Buscar
         a.button.danger &times;
+
+    p
+      small {{ searchMessage }}
+
+    ul
+      li(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Creep', artist: 'Radiohead' },
+  { name: 'The Verve', artist: 'The Symphony' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `${this.tracks.length} results`
     }
   },
   methods: {
     search () {
-      console.log(this.searchQuery)
+      this.tracks = tracks
     }
   }
 }
