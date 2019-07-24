d6f752e3fc2b059ca0f94b275422aab632c82d92
Daniel H <daguiheso@gmail.com>
2019-07-23 20:57:45 -0500

    separe scss

--- src/App.vue
+++ src/App.vue
@@ -47,35 +47,5 @@ export default {
 </script>
 
 <style lang="scss">
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    padding: 5%;
-  }
-  nav {
-    display: flex;
-    input {
-      height: 45px;
-      padding: 12px;
-      width: 100%;
-      border-radius: 6px;
-    }
-    a.button {
-      height: 45px;
-      padding: 0 22px;
-      color: white;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-radius: 6px;
-      cursor: pointer;
-      &.info {
-        background: rgb(50, 60, 207);
-      }
-      &.danger {
-        background: rgb(233, 38, 71);
-      }
-    }
-  }
+  @import './scss/main.scss';
 </style>
