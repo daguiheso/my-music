d28c6c6a62d9114798180e2629622052df5f48d7
Daniel H <daguiheso@gmail.com>
2019-07-24 23:37:34 -0500

    Create component track

---
+++ src/components/Track.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+	.card
+		.card-image
+			figure.image
+				img()
+		.card-body
+			.media
+				figure.image
+				.media-content
+					h4.title
+					h5.subtitle
+			.content
+				small
+				nav.level
+					a
+						span play
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
